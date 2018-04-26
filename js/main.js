d4f6dda788468e09a04a221572e49984de77ecca
Unknown <damienvignes@hotmail.com>
2018-04-26 21:47:44 +0200

    commencement modal & block chats

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+// ----- Preloader -------
 $(document).ready(function() {
 
 	setTimeout(function(){
@@ -5,3 +7,23 @@ $(document).ready(function() {
 	}, 3000);
 
 });
+
+// ------ Modal ---	---
+
+var modal = document.getElementById('myModal')
+	,	btn = document.getElementById('btn')
+	, span = document.getElementsByClassName("close")[0]
+
+	btn.onclick = function() {
+		modal.style.display = "block"
+	}
+
+	span.onclick = function() {
+		modal.style.display = "none"
+	}
+
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none"
+		}
+	}
